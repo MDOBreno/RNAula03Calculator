f40284fc239100e5757f7d07be2495762a9c45f9
Breno Medeiros De Oliveira <>
2023-11-07 23:46:43 -0300

    - Secao04Aula63 - Logica da Calculadora 02

--- src/PlaygroundDeJsDeBreno.js
+++ src/PlaygroundDeJsDeBreno.js
@@ -6,3 +6,14 @@ console.log(!!''); //String vazia: false
 console.log(!!' '); //String nao vazia: true
 console.log(!!0); //Numero zero: false
 console.log(!!-1); //Numero nao zero: true
+
+
+const values = [23, 2];
+console.log(values);
+const operation = '+';
+console.log(operation);
+console.log(eval(`${values[0]} ${operation} ${values[1]}`));
+const operation2 = '=';
+console.log(eval(`${values[0]} ${operation2} ${values[1]}`));
+
+
