84323bea2f92f3bb6d6106b90d8d28a24919a604
Breno Medeiros De Oliveira <>
2023-11-07 23:28:21 -0300

    - Secao04Aula61e62 - Logica da Calculadora 01

--- App.js
+++ App.js
@@ -18,22 +18,41 @@ import {
 import Button from './src/components/Button';
 import Display from './src/components/Display';
 
+const initialState = {
+  displayValue: '0',
+  clearDisplay: false,
+  operation: null,
+  values: [0, 0],
+  current: 0,
+}
 
 export default class App extends Component {
-  state = {
-    displayValue: '0'
-  }
+  state = { ...initialState }
 
   addDigit = n => {
+    if (n=='.' && this.state.displayValue.includes('.')) {
+      return;
+    }
+
+    const clearDisplay = this.state.displayValue === '0'
+      || this.state.clearDisplay;
+    const currentValue = clearDisplay ? '' : this.state.displayValue;
+    const displayValue = currentValue + n;
     this.setState({
-        displayValue: n
+        displayValue, clearDisplay: false
       }
     )
+    if (n !== '.') {
+      const newValue = parseFloat(displayValue);
+      const values = [...this.state.values];
+      values[this.state.current] = newValue;
+      this.setState({values});
+    }
   }
 
   clearMemory = () => {
     this.setState ({
-        displayValue: '0'
+        ...initialState
       }
     )
   }
